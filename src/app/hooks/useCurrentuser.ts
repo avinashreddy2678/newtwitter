8ef29a62e73afea089ee84b88f016cbca5edb52f
avinash <avinashreddie777@gmail.com>
2023-11-23 10:03:28 +0530

    changed

--- src/app/hooks/useCurrentuser.ts
+++ src/app/hooks/useCurrentuser.ts
@@ -1,15 +1,32 @@
 "use client";
-import useSWR from 'swr';
+import useSWR from "swr";
 import fetcher from "../lib/fetcher";
+import axios from "axios";
+import { useEffect, useState } from "react";
 const useCurrentUser = () => {
-    const { data, error, isLoading, mutate } = useSWR('/api/users/currentuser', fetcher);
-    //console.log(!isLoading && data);
+  const [data,setdata]=useState([])
+  const {  error, isLoading, mutate } = useSWR(
+    "/api/users/currentuser",
+    fetcher
+  );
+  // const response =  axios.get('/api/users/currentuser');
+  //console.log(!isLoading && data);
+
+  const getdata = async () => {
+    const response = await axios.get("/api/users/currentuser");
+    setdata(response.data)
+    console.log(response.data);
+  };
+  useEffect(() => {
+    getdata();
+  }, [mutate]);
+
   return {
     data,
     error,
     isLoading,
-    mutate
-  }
+    mutate,
+  };
 };
 
 export default useCurrentUser;
