417e51bb2f050d91e0f842ab1f55c75c97645c9a
avinash <avinashreddie777@gmail.com>
2023-11-23 14:18:28 +0530

    changed

--- src/app/hooks/useCurrentuser.ts
+++ src/app/hooks/useCurrentuser.ts
@@ -1,7 +1,8 @@
 'use client';
 import useSWR from 'swr';
-import fetcher from "../lib/fetcher";
+// import fetcher from "../lib/fetcher";
 const useCurrentUser = () => {
+  const fetcher = (...args:any) => fetch(...args,{cache:'no-store'}).then((res) => res.json())
     const { data, error, isLoading, mutate } = useSWR('/api/users/currentuser', fetcher);
     //console.log(!isLoading && data);
   return {
